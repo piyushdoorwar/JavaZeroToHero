000fa5f22b98d6f3a11c5c0c57d55ea4bda14926
Piyush Doorwar <piyushdoorwar@gmail.com>
2024-12-16 12:29:36 +0530

    Delete src/src directory

--- src/src/AddNumbersMethod.java
+++
@@ -1,9 +0,0 @@
-public class AddNumbersMethod {
-    public static int addNumbers(int a, int b){
-        return a + b;
-    }
-    public static void main(String[] args) {
-        int sum = addNumbers(5, 10);
-        System.out.println("Sum: " + sum);
-    }
-}
